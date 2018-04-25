dfcdc1711fc288af9d1598b947c9334294c62a9e
Ioannis Kolovos <yannis.kolovos@gmail.com>
2018-04-26 00:11:21 +0300

    fix AudioContext and another refactorings

--- extension/js/app.js
+++ extension/js/app.js
@@ -13,7 +13,7 @@
 		
 'use strict';
 
-var internalIpAddress, apiBaseUrl, username, bridge_status= null;
+var frequencyArray,internalIpAddress, apiBaseUrl,username,bridge_status,audioContent,path, paths,visualizer,mask,audioStream,analyser= null;
 var deviceType     = {devicetype: "RetroHue#WebClient"};
 var controls       = ['saturation', 'brightness', 'sensitivity', 'transitiontime'];
 var transitiontime = 1;
@@ -22,45 +22,65 @@ var saturation     = 250;
 var brightness     = 250;
 var activeRequest  = false;
 var selectedLamps  = [];
+var lamps          = {};
 var hueCollection  = 65000;
 var enabled        = true;
+var bridges        = [];
+var selectedBridge = 0;
+var bridge_status  = "Philips Hue Bridge: Not Connected";
 
 
-
-var	retroHue = function () {
-	if ('https:' == location.protocol)
-		alert('Running on a demo mode!\n Please install the Chrome extension to manage Philips Hue Lamps')
+var	setUpRetroHue = function () {
 	
-
+	if ('https:' == location.protocol) alert('Running on a demo mode!\n Please install the Chrome extension to manage Philips Hue Lamps');
+	
 	$.getJSON("https://www.meethue.com/api/nupnp", function(data) {
-
-		if (data.length == 0) { alert("We can't find Philips Bridge"); }
-		else { internalIpAddress = data[0].internalipaddress;	 }
-
-	}).done(function() {
-
-		authorizeUser();
-		apiBaseUrl = "http://" + internalIpAddress + "/api/" + username;
-
-	}).done(function() {
-
-		if (username !== undefined) selectLights();
-
-		bridge_status = (username !== undefined) ? ("Connected to "+ internalIpAddress +", using "+selectedLamps.length+ " lambs") : (" Not Connected");
-		$('.status').html("Philips Hue Bridge:" + bridge_status);
+		if (data instanceof Array  && data.length > 0) { 
+			bridges = data
+			internalIpAddress = bridges[selectedBridge].internalipaddress;	 
+		}
+		else { 
+			alert("We can't find Philips Bridge"); 
+		}
 
 	}).done(function() {
+		if (bridges.length > 0) {
+			authorizeUser();
+			apiBaseUrl = "http://" + internalIpAddress + "/api/" + username;
+		
+			if (username !== undefined) {
+				selectLights();
+				bridge_status = "Connected to  "+ getBridge().id +" at "+ getBridge().internalipaddress +", using "+selectedLamps.length+ " lambs";
+				$('.status').html("Philips Hue Bridge:" + bridge_status);
+			}
+		}
+		
+		
+		$('svg').fadeIn( "slow");
+	});
 
-		$.each(controls, function(i, control) {
-			initializeControl(control);
-		});
+	
+	$.each(controls, function(i, control) {
+		initializeControl(control);
 	});
 }
 
 
+var	getBridge = function() {
+	return bridges[selectedBridge]
+}
+
+
+
+var	setUpSelectors = function () {
+	paths          = document.getElementsByTagName('path');
+	visualizer     = document.getElementById('visualizer');
+	mask           = visualizer.getElementById('mask');
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+}
+
 
 var authorizeUser = function () {
-
 	if (localStorage.getItem('username') !== null) {
 		username = localStorage.getItem('username');
 		return true
@@ -88,19 +108,22 @@ var selectLights = function () {
 		url:  apiBaseUrl + "/lights",
 		type: "GET"
 	}).done(function(data) {
-
 		if (data instanceof Object) {
+			lamps = data;
+			
 			var ids = Object.keys(data);
-
 			alert("We found " + ids.length + " lamps in your network "+internalIpAddress+". \n Please select the lamps you want to use! \n Note: We will turn on the lamp for you if its closed");
+			
 			$.each(ids, function(index, id) {
 				if (confirm("Do you want to use: " + data[id].name + " (" + data[id].modelid+" - "+data[id].type+")")) {
 					// open the light if its closed
-					if (!data[id].state.on) setLightState(id, {on: true}) 
-						selectedLamps.push(id)
+					if (!data[id].state.on) setLightState(id, {on: true})
+						selectedLamps.push(id);
 				}
 			});
 		}
+		
+		
 	});
 }
 
@@ -116,14 +139,11 @@ var initializeControl = function (at) {
 	if (localStorage.getItem(at)){
 		selector.val(localStorage.getItem(at)).trigger('input');
 	}
-
-}	
+};	
 
 var setLightState = function(lightId, lightState) {
 	if (activeRequest || !enabled || selectedLamps.length == 0 || !username) return;
-
 	var apiUrl = apiBaseUrl + "/lights/" + lightId + "/state";
-
 	activeRequest = true
 
 	$.when( 
@@ -137,28 +157,18 @@ var setLightState = function(lightId, lightState) {
 	};
 
 	// adapted from https://codepen.io/zapplebee/pen/gbNbZE
-	window.onload = function () {    
-		var paths = document.getElementsByTagName('path');
-		var visualizer = document.getElementById('visualizer');
-		var mask = visualizer.getElementById('mask');
-		var h = document.getElementsByTagName('h1')[0];
-		var path;
-		var report = 0;
-
 		var soundAllowed = function (stream) {
 			//Audio stops listening in FF without // window.persistAudioStream = stream;
 			//https://bugzilla.mozilla.org/show_bug.cgi?id=965483
 			//https://support.mozilla.org/en-US/questions/984179
+			audioContent.resume();
 			window.persistAudioStream = stream;
-			h.innerHTML = "Thanks";
-			h.setAttribute('style', 'opacity: 0;');
-			var audioContent = new AudioContext();
-			var audioStream = audioContent.createMediaStreamSource( stream );
-			var analyser = audioContent.createAnalyser();
+			audioStream = audioContent.createMediaStreamSource( stream );
+			analyser = audioContent.createAnalyser();
 			audioStream.connect(analyser);
 			analyser.fftSize = 1024;
 
-			var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
+			frequencyArray = new Uint8Array(analyser.frequencyBinCount);
 			visualizer.setAttribute('viewBox', '0 0 255 255');
 
 			//Through the frequencyArray has a length longer than 255, there seems to be no
@@ -168,40 +178,48 @@ var setLightState = function(lightId, lightState) {
 				path.setAttribute('stroke-dasharray', '4,1');
 				mask.appendChild(path);
 			}
-			var doDraw = function () {
-				requestAnimationFrame(doDraw);
-				analyser.getByteFrequencyData(frequencyArray);
-				var adjustedLength;
-				for (var i = 0 ; i < 255; i++) {
-				
-					adjustedLength = Math.floor(frequencyArray[i]) - (Math.floor(frequencyArray[i]) % 5);
-					paths[i].setAttribute('d', 'M '+ (i) +',255 l 0,-' + adjustedLength);
-
-					if (adjustedLength > sensitivity) {
-						var id  = selectedLamps[Math.floor(Math.random() * selectedLamps.length)];
-						
-						setLightState(id, { 
-							hue: Math.round(Math.random() * hueCollection), 
-							transitiontime: transitiontime,
-							sat: saturation,
-							bri: brightness
-						})
-					}
-				}
-
-			}
+			
+			setUpRetroHue();			
 			doDraw();
-		}
+	};
 
-		var soundNotAllowed = function (error) {
-			h.innerHTML = "You must allow your microphone.";
-			console.log(error);
-		}
 
-		navigator.getUserMedia({audio:true}, soundAllowed, soundNotAllowed);
+
+	var doDraw = function () {
+		requestAnimationFrame(doDraw);
+		analyser.getByteFrequencyData(frequencyArray);
+		var adjustedLength;
+		for (var i = 0 ; i < 255; i++) {
+		
+			adjustedLength = Math.floor(frequencyArray[i]) - (Math.floor(frequencyArray[i]) % 5);
+			paths[i].setAttribute('d', 'M '+ (i) +',255 l 0,-' + adjustedLength);
+
+			if (adjustedLength > sensitivity) {
+				var id  = selectedLamps[Math.floor(Math.random() * selectedLamps.length)];
+				
+				setLightState(id, { 
+					hue: Math.round(Math.random() * hueCollection), 
+					transitiontime: transitiontime,
+					sat: saturation,
+					bri: brightness
+				})
+			}
+		}
 	};
 	
+	var soundNotAllowed = function (error) {
+		console.log(error);
+	}
 	
+
 	$(function() { 
-		retroHue();
-	});
+		$( "#start" ).one( "click", function() {
+			$('#start').hide();
+			audioContent = new AudioContext();
+			setUpSelectors();
+			$('#footer').fadeIn("slow")
+			$('#logo').fadeIn("slow", function() {
+				navigator.getUserMedia({audio:true}, soundAllowed, soundNotAllowed);				
+			});
+		});
+	});	
